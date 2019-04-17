68da4a41ca78f663a1a5e689bbb92a1613f8bf13
hy949 <1661346771@qq.com>
2019-04-17 19:44:24 +0800

    配置hello路由

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
+import router from 'vue-router';
+import HelloWorld from "./components/HelloWorld";
 
+Vue.use(router)
 Vue.config.productionTip = false
+
+//配置路由
+var rt = new router({
+  routes:[{
+    path: '/hello',
+    component: HelloWorld
+  }]
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router: rt,
   components: { App },
   template: '<App/>'
 })
